bceabb5a47de2a8fb2d79f6257c69944d45b66ae
ftptpf <Nb49Rtdd>
2022-08-10 20:38:37 +0300

    3. LazyInitializationexception [#331987 #330662] - Fetch

--- src/main/java/ru/job4j/run/lazy/HbmLazyFetchCarRun.java
+++ src/main/java/ru/job4j/run/lazy/HbmLazyFetchCarRun.java
@@ -8,27 +8,28 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.model.lazy.CarBrandL;
 import ru.job4j.model.lazy.CarModelL;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class HbmLazyCarRun {
+public class HbmLazyFetchCarRun {
     public static void main(String[] args) {
+        List<CarBrandL> carBrandsListL = new ArrayList<>();
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
-            List<CarBrandL> carBrandsListL = session.createQuery("from CarBrandL").list();
-            for (CarBrandL brand : carBrandsListL) {
-                for (CarModelL model : brand.getModels()) {
-                    System.out.println(model);
-                }
-            }
+            carBrandsListL = session.createQuery("select distinct c from CarBrandL c join fetch c.models").list();
+
             session.getTransaction().commit();
             session.close();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             StandardServiceRegistryBuilder.destroy(registry);
+        }
+        for (CarModelL model : carBrandsListL.get(0).getModels()) {
+            System.out.println(model);
         }
     }
 }
